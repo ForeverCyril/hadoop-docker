d779e81aaece6045e0b0b27bde8116b0da7a8c40
ForeverCyril <cyrilchen1999@outlook.com>
2022-04-01 00:00:57 +0800

    fix: remove 'sudo' in stop.sh

--- stop.sh
+++ stop.sh
@@ -2,10 +2,11 @@
 
 N=${1:-3}
 
-sudo docker rm -f hadoop-master &> /dev/null
+docker rm -f hadoop-master &> /dev/null
+
 i=1
 while [ $i -le $N ]
 do
-	sudo docker rm -f hadoop-worker$i &> /dev/null
+	docker rm -f hadoop-worker$i &> /dev/null
 	i=$(( $i + 1 ))
 done 
